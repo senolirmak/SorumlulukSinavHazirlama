470482b9529fa13ad7647ed60dbcfe1f6ea3f1e8
Şenol Irmak <senolirmak@gmail.com>
2023-10-20 22:57:36 +0300

    Message explaining Şenol changes

--- eOkulxlsxDuzenle.py
+++ eOkulxlsxDuzenle.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug 30 15:39:07 2023
-
+Sorumlu Dersi Olan Öğrenciler sadece veri olarak indirilir
+OOK12001R012_91.xlsx dosyası
 @author: senolirmak
 """
 from src.veriduzenle_version01 import veriDuzenle
